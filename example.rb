457fa1bb84bf7f920ed05f67d3078d2eb8e454f1
Jean de Klerk <jadekler@gmail.com>
2016-03-15 20:41:04 -0600

    PUT/DELETE response, GET/DELETE request done

--- example.rb
+++ example.rb
@@ -1,5 +1,5 @@
 require_relative 'lib/http_sim'
 
 HttpSimulator.register_endpoint 'GET', '/hi', 'yasssss'
-HttpSimulator.register_endpoint 'GET', '/bye', 'byeeeee'
+HttpSimulator.register_endpoint 'POST', '/bye', 'byeeeee'
 HttpSimulator.run!
--- lib/http_sim.rb
+++ lib/http_sim.rb
@@ -11,7 +11,8 @@ def read_file(path)
 end
 
 class Endpoint
-  attr_reader :method, :path, :default_response
+  attr_reader :method, :path, :default_response, :response
+  attr_accessor :requests
 
   @@supported_methods = ['GET', 'PUT', 'PATCH', 'POST', 'DELETE']
 
@@ -20,7 +21,13 @@ class Endpoint
 
     @method = method
     @path = path
+    @response = default_response
     @default_response = default_response
+    @requests = []
+  end
+
+  def add_request(request)
+    @requests.push request
   end
 end
 
@@ -46,46 +53,49 @@ module HttpSimulator
     case endpoint.method
       when 'GET'
         Sinatra::Base.get endpoint.path do
+          endpoint.add_request request.body.read
           endpoint.default_response
         end
       when 'PUT'
         Sinatra::Base.put endpoint.path do
+          endpoint.add_request request.body.read
           endpoint.default_response
         end
       when 'PATCH'
         Sinatra::Base.patch endpoint.path do
+          endpoint.add_request request.body.read
           endpoint.default_response
         end
       when 'POST'
         Sinatra::Base.post endpoint.path do
+          endpoint.add_request request.body.read
           endpoint.default_response
         end
       when 'DELETE'
         Sinatra::Base.delete endpoint.path do
+          endpoint.add_request request.body.read
           endpoint.default_response
         end
     end
 
     Sinatra::Base.get "#{endpoint.path}/response" do
-      @endpoint = endpoint
       ERB.new(read_file('lib/response.html.erb')).result binding
     end
 
     Sinatra::Base.put "#{endpoint.path}/response" do
-
+      endpoint.response = request.body.read
     end
 
     Sinatra::Base.delete "#{endpoint.path}/response" do
-
+      endpoint.response = endpoint.default_response
     end
 
     Sinatra::Base.get "#{endpoint.path}/requests" do
-      @endpoint = endpoint
       ERB.new(read_file('lib/request.html.erb')).result binding
     end
 
     Sinatra::Base.delete "#{endpoint.path}/requests" do
-
+      endpoint.requests = []
     end
   end
 end
