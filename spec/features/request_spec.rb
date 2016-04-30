d82f95baf2d50cca1f8a6de4e50bc02261fe025a
Jean de Klerk <jadekler@gmail.com>
2016-04-29 18:05:36 -0600

    Updated spec format, added missing specs

--- spec/features/request_spec.rb
+++ spec/features/request_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe '/<endpoint>/requests' do
+describe 'requests' do
   before :each do
     HttpSimulator.reset_endpoints
   end
@@ -9,26 +9,36 @@ describe '/<endpoint>/requests' do
     HttpSimulator.stop_daemon!
   end
 
-  it 'sets up a /request endpoint for each registered endpoint' do
-    HttpSimulator.register_endpoint 'POST', '/hi', ''
-    HttpSimulator.register_endpoint 'POST', '/bye', ''
-    HttpSimulator.run_daemon!(port: 6565)
+  describe 'getting requests' do
+    it 'sets up GET /<endpoint>/requests for each registered endpoint' do
+      HttpSimulator.register_endpoint 'POST', '/hi', ''
+      HttpSimulator.register_endpoint 'POST', '/bye', ''
+      HttpSimulator.run_daemon!(port: 6565)
 
-    resp = HTTParty.get('http://localhost:6565/hi/requests')
-    expect(resp.code).to eq 200
+      resp = HTTParty.get('http://localhost:6565/hi/requests')
+      expect(resp.code).to eq 200
 
-    resp = HTTParty.get('http://localhost:6565/bye/requests')
-    expect(resp.code).to eq 200
+      resp = HTTParty.get('http://localhost:6565/bye/requests')
+      expect(resp.code).to eq 200
+    end
+
+    it 'GET /<endpoint>/requests returns recorded requests' do
+      HttpSimulator.register_endpoint 'POST', '/bye', 'byeeeee'
+      HttpSimulator.run_daemon!(port: 6565)
+
+      HTTParty.post('http://localhost:6565/bye', :body => 'hello world')
+
+      resp = HTTParty.get('http://localhost:6565/bye/requests')
+      expect(resp.code).to eq 200
+      expect(resp.body).to include 'hello world'
+    end
+
+    xit '.resets returns recorded requests' # TODO
   end
 
-  it 'records requests at /requests endpoint' do
-    HttpSimulator.register_endpoint 'POST', '/bye', 'byeeeee'
-    HttpSimulator.run_daemon!(port: 6565)
-
-    HTTParty.post('http://localhost:6565/bye', :body => 'hello world')
-
-    resp = HTTParty.get('http://localhost:6565/bye/requests')
-    expect(resp.code).to eq 200
-    expect(resp.body).to include 'hello world'
+  describe 'resetting requests' do
+    xit 'sets up a DELETE /<endpoint>/requests for each registered endpoint' # TODO
+    xit 'DELETE /<endpoint>/requests resets recorded requests' # TODO
+    xit '.reset_requests reset recorded requests' # TODO
   end
 end
--- spec/features/response_spec.rb
+++ spec/features/response_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe '/<endpoint>/response' do
+describe 'responses' do
   before :each do
     HttpSimulator.reset_endpoints
   end
@@ -9,15 +9,32 @@ describe '/<endpoint>/response' do
     HttpSimulator.stop_daemon!
   end
 
-  it 'sets up a /response endpoint for each registered endpoint' do
-    HttpSimulator.register_endpoint 'POST', '/hi', ''
-    HttpSimulator.register_endpoint 'POST', '/bye', ''
-    HttpSimulator.run_daemon!(port: 6565)
+  describe 'getting response' do
+    it 'sets up GET /<endpoint>/response for each registered endpoint' do
+      HttpSimulator.register_endpoint 'POST', '/hi', ''
+      HttpSimulator.register_endpoint 'POST', '/bye', ''
+      HttpSimulator.run_daemon!(port: 6565)
 
-    resp = HTTParty.get('http://localhost:6565/hi/response')
-    expect(resp.code).to eq 200
+      resp = HTTParty.get('http://localhost:6565/hi/response')
+      expect(resp.code).to eq 200
 
-    resp = HTTParty.get('http://localhost:6565/bye/response')
-    expect(resp.code).to eq 200
+      resp = HTTParty.get('http://localhost:6565/bye/response')
+      expect(resp.code).to eq 200
+    end
+
+    xit 'GET /<endpoint>/response returns response for endpoint' # TODO
+    xit '.response returns response for endpoint' # TODO
+  end
+
+  describe 'setting response' do
+    xit 'sets up PUT /<endpoint>/response for each registered endpoint' # TODO
+    xit 'PUT /<endpoint>/response alters response for endpoint' # TODO
+    xit '.set_response alters response for endpoint' # TODO
+  end
+
+  describe 'resetting response' do
+    xit 'sets up a DELETE /<endpoint>/response for each registered endpoint' # TODO
+    xit 'DELETE /<endpoint>/response resets response for endpoint' # TODO
+    xit '.reset_response resets response for endpoint' # TODO
   end
 end
