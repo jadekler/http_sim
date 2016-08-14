9e4f3107ea11e9f18f1a2b4ddacc305eed7345c9
Jean de Klerk <jadekler@gmail.com>
2016-08-14 10:41:53 -0600

    app_wrapper.new.helpers does not work

--- lib/sim.rb
+++ lib/sim.rb
@@ -4,15 +4,16 @@ module HttpSimulator
   class Sim < Sinatra::Base
     set :port, 6565
 
-    def check
-      'yep'
-    end
-
     def register_endpoint(method: 'GET', endpoint: '/default_endpoint', body: 'Default body')
+      @endpoints = [] if @endpoints == nil
+
+      @endpoints.push({method: method, endpoint: endpoint, body: body})
     end
 
-    get '/' do
-      puts 'Gotcha'
+    post '/*' do
+      puts '*'*80
+      p @endpoints
+      puts '*'*80
     end
   end
 end
--- spec/features/register_endpoint_spec.rb
+++ spec/features/register_endpoint_spec.rb
@@ -4,23 +4,19 @@ describe '/<endpoint>' do
   before :each do
     @sim = HttpSimulator.start_sim(port: test_port)
 
-    # @sim.reset_endpoints
-
     @sim.register_endpoint(method: 'GET', endpoint: '/foo', body: 'something')
-    @sim.register_endpoint(method: 'POST', endpoint: '/bar', body: 'something else')
-
-    # @sim.run_daemon!(port: test_port)
+    @sim.register_endpoint(method: 'POST', endpoint: '/bar/gaz', body: 'something else')
   end
 
   after :each do
-    # @sim.stop_daemon!(port: test_port)
+    # @sim.stop
   end
 
   it 'sets up an endpoint for each registered endpoint' do
-    resp = get '/foo'
-    expect(resp.code).to eq 200
+    # resp = get '/foo'
+    # expect(resp.code).to eq 200
 
-    resp = post '/bar', 'some inconsequential json'
+    resp = post '/bar/gaz', 'some inconsequential json'
     expect(resp.code).to eq 200
   end
 end
